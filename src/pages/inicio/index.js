1c3263fafa4b4fe84b13c6b7849b46d38d9702a3
DNSO0 <toxicthrashd@gmail.com>
2025-01-01 11:33:31 -0500

    Componente Banner con estilos basicos

--- src/pages/inicio/index.js
+++ src/pages/inicio/index.js
@@ -1,8 +1,16 @@
+import Banner from "../../components/Banner";
 import Cabecera from "../../components/Cabecera/Cabecera";
+import Pie from "../../components/Pie";
 
 function Inicio(){
     return(
-        <Cabecera />
+        <>
+            <Cabecera />
+          
+            <Banner img="home" color="#154580" />
+    
+            <Pie />
+        </>
     )
 }
 export default Inicio;
