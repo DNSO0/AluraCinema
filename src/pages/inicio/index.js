b5381d4e59fa1200ac89cb869bb8d4e38a3799dd
DNSO0 <toxicthrashd@gmail.com>
2025-01-02 12:29:21 -0500

    Componente Titulo

--- src/pages/inicio/index.js
+++ src/pages/inicio/index.js
@@ -1,14 +1,16 @@
 import Banner from "../../components/Banner";
 import Cabecera from "../../components/Cabecera/Cabecera";
 import Pie from "../../components/Pie";
+import Titulo from "../../components/Titulo";
 
 function Inicio(){
     return(
         <>
-            <Cabecera />
-          
+            <Cabecera />          
             <Banner img="home" color="#154580" />
-    
+            <Titulo>
+                <h1>Un lugar para guardar sus videos favoritos</h1>
+            </Titulo>
             <Pie />
         </>
     )
