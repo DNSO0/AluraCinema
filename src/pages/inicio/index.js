53aef306ff6da5fe9e9ae256b5213a390faf6af3
DNSO0 <toxicthrashd@gmail.com>
2025-01-02 12:47:02 -0500

    Componente Card, estructura basica

--- src/pages/inicio/index.js
+++ src/pages/inicio/index.js
@@ -2,7 +2,8 @@ import Banner from "../../components/Banner";
 import Cabecera from "../../components/Cabecera/Cabecera";
 import Pie from "../../components/Pie";
 import Titulo from "../../components/Titulo";
-
+import Card from "../../components/Card";
+ 
 function Inicio(){
     return(
         <>
@@ -11,6 +12,7 @@ function Inicio(){
             <Titulo>
                 <h1>Un lugar para guardar sus videos favoritos</h1>
             </Titulo>
+            <Card id="1" titulo="Gato" capa="https://api.thecatapi.com/api/images/get?format=src&ype=png"/>
             <Pie />
         </>
     )
